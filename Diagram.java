8890105196c60a337bf26d09988a4ccddc7bcb64
NaveenYalamati <yalamatinaveen1@gmail.com>
2021-12-06 19:51:35 +0530

    my task 3

---
+++ Diagram.java
@@ -0,0 +1,13 @@
+
+public class Diagram {
+
+	public static void main(String[] args) {
+		int a = 1, b = 2, c = 3, perimeter, h = 5, area;
+		perimeter = a + b + c;
+		System.out.println(perimeter);
+		area = (h * b) / 2;
+		System.out.println(area);
+
+	}
+
+}
